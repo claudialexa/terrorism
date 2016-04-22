81e5cf23e5ba3ff1272d1ddc01db62f9c6071d66
claudia aguirre <claudialexaa@gmail.com>
2016-04-22 17:07:38 -0400

    Line graph
    hi this is the line graph

--- js/line-graph.js
+++ js/line-graph.js
@@ -1,46 +1,49 @@
 
 	d3.csv("data/terrorism-clean-data.csv", function (error, data) {
 
+		var yearFormat = d3.time.format("%Y");
+
 		if (error) {
 			console.log("error reading file");
 		}
 
+	// BEGIN: MAKECHART1 FUNCTION. ALL UNIVERSAL VARIABLES
+
 	function makeChart1() {
 
-	var fullwidth = 1000;
-	var fullheight = 500;
-	var chart1Color = d3.scale.ordinal()
+	var fullwidth = 1200;
+	var fullheight = 600;
 
- 	var yearFormat = d3.time.format("%Y");
+	var chart1Color = d3.scale.ordinal()
 	var chart1margin = {top: 20, right: 25, bottom: 20, left: 100};
 
-	var width = fullwidth- chart1margin.left - chart1margin.right, 
+	var width = fullwidth- chart1margin.left - chart1margin.right,
 		height = fullheight - chart1margin.top - chart1margin.bottom;
 
 	var xScale = d3.time.scale().range([0, width]);
-	var yScale = d3.scale.linear().range([0, height]);
+	var yScale = d3.scale.linear()
+				.range([0, height])
+				// .domain([-2,80]);
 
 	var xAxis = d3.svg.axis()
 		.scale(xScale)
 		.orient("bottom")
-		.ticks(13)
+		.ticks(15)
 		.innerTickSize([0]);
 
 	var yAxis = d3.svg.axis()
 		.scale(yScale)
 		.orient("left")
+		.ticks(15)
 		.innerTickSize([0]);
-
-	// Begin, Problem
 
 	var line = d3.svg.line()
 		.x(function (d) {
-			return xScale(yearFormat.parse(d.key)); //d.key is year
+			return xScale(yearFormat.parse(d.key));
 		})
 		.y(function (d) {
 			return yScale(d.values.incidents);
 		});
-	// End, Problem
 
 	var chart1 = d3.select("#chart1")
 		.append("svg")
@@ -53,116 +56,118 @@
   	.append("div")
   	.attr("class", "tooltip");
 
-	// var years = ["1970", "1971", "1972", "1973", "1974", "1975", "1976", "1977", "1978", "1979", "1980", "1981", "1982", "1983", "1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014"];
 
-	//BEGIN: Fixing Data
+  	// END: MAKECHART1 FUNCTION. ALL UNIVERSAL VARIABLES
 
-  	var countryByYear = d3.nest()
-     .key(function(d) { return d.country_txt; })
-     .sortKeys(d3.ascending)
-     .key(function(d) {return d.iyear;})
-     .sortKeys(function(a,b) {
-     	return d3.ascending(yearFormat.parse(a), yearFormat.parse(b));
-     })
-     .rollup(function(leaves) { return { "incidents": leaves.length, "deaths": d3.sum(leaves, function(d) { return +d.nkill; })
-          };
-      })
-      .entries(data);
+	//CURRENT DATA//
 
-	console.log(countryByYear);
+	var countryData = makeCountryData(data);
+	drawGraph(countryData);
 
-	var totalIncidents = d3.nest()
-	  .key(function(d) { return d.country_txt; })
-	  // .key(function(d) { return d.iyear; })
-	  .rollup(function(v) { return v.length; })
-	  .entries(data);
+	console.log(countryData);
 
-	  console.log(totalIncidents);
+	//BEGIN: BUTTONS
 
-    function top50countries(totalIncidents) {
-        return totalIncidents.sort(function (a, b) {
-            return d3.descending(a.values, b.values);
-        }).slice(0,100);
-    }
+	d3.select("button#groups").on("click", function() {
+		var groupData = makeGroupData(data);
+		drawGraph(groupData);
+	});
 
-    var topIncidents = top50countries(totalIncidents);
-    console.log(topIncidents);
+	d3.select("button#countries").on("click", function() {
+		var groupData = makeCountryData(data);
+		drawGraph(countryData);
+	});
 
-   	var topCountriesByIncidentsNames = topIncidents.map(function (d) {return d.key;}); // country names
+	//END: BUTTONS
 
-		var topCountriesForLine = countryByYear.filter(function(d) {
-			return topCountriesByIncidentsNames.indexOf(d.key) !== -1;
-		});
+	//BEGIN: DRAWGRAPH FUNCTION
 
-		// structure is key: country, { key: year, values: deaths incident}
+	function drawGraph(data) {
 
-		var valuesForXScale = d3.merge(topCountriesForLine.map(function(d) {return d.values;}));
-		var valuesForYScale = valuesForXScale.map(function(d) {return d.values;});
+		var groups = chart1.selectAll("g.lines")
+			.data(data);
 
-	//END: Fixing Data
+		groups
+			.enter()
+			.append("g")
+			.attr("class", "lines")
+			.on("mouseover", mouseoverFunc)
+			.on("mousemove", mousemoveFunc)
+			.on("mouseout",	mouseoutFunc);
 
-	// Begin: Making Scales (Problem)
+		groups.exit()
+			.remove();
 
-	xScale.domain(
-		d3.extent(valuesForXScale, function(d) {
-			return yearFormat.parse(d.key);
-		}));
+		var lines = groups.selectAll("path")
+			.data(function(d) {
+				return [ d.values ];
+			});
+
+			lines
+			.enter()
+			.append("path")
+			.attr("class", "line");
+
+			lines.transition()
+			.duration(1000)
+			.attr("d", line); 
 
 
-	yScale.domain([
-		d3.max(valuesForYScale, function(d) {
-			return d.incidents;
-		}),
-		0
-		]);
+			lines.exit().remove();
 
-	// End: Making Scales (Problem)
+		var circles = groups.selectAll("circle")
+			.data(function(d) {
+				return d.values;
+			});
 
-	var groups = chart1.selectAll("g.lines")
-		.data(topCountriesForLine)
-		.enter()
-		.append("g")
-		.attr("class", "lines");
+			circles
+			.enter()
+			.append("circle");
 
-	groups.selectAll("path")
-		.data(function(d) {
-			return [ d.values ];
-		})
-		.enter()
-		.append("path")
-		.attr("class", "line")
-		//.style("stroke", function(d) {
-		//	return chart1Color(d[0].Party); })
-		.attr("d", line); 
-
-	var circles = groups.selectAll("circle")
-		.data(function(d) { 
-			return d.values; 
-		})
-		.enter()
-		.append("circle");
-
-	circles.attr("cx", function(d) {
+			circles.transition()
+			.attr("cx", function(d) {
 				return xScale(yearFormat.parse(d.key));
 			})
 			.attr("cy", function(d) {
 				return yScale(+d.values.incidents);
 			})
-			.style("opacity", 0.8)
-			//.style("fill", function(d) {
-			//	return chart1Color(d.Party);
-			//})
-			.attr("r", 4); 
+			.duration(2000)
+			.style("opacity", 0.5)
+			.attr("r", 2);
 
-		circles
-			.on("mouseover", mouseoverFunc)
-			.on("mousemove", mousemoveFunc)
-			.on("mouseout",	mouseoutFunc);
+		circles.exit().remove();
 
-  	// Still need to fix line label ugh these go here
+		function mouseoverFunc(d) {
+		d3.select(this)
+			.transition()
+			.style("opacity", 1)
+			.attr("stroke", 10);
+		tooltip
+			.style("display", null) // this removes the display none setting from it
+			.html("<p>" + d.key+ "</p>");
+		}
+
+	function mousemoveFunc(d) {
+		tooltip
+			.style("top", (d3.event.pageY - 10) + "px" )
+			.style("left", (d3.event.pageX + 10) + "px");
+		}
+
+	function mouseoutFunc(d) {
+		d3.select(this)
+			.transition()
+			.style("opacity", 1)
+			.attr("r", 4);
+    	tooltip.style("display", "none");  // this sets it to invisible!
+
+     }
 
 
-  	//Axes
+  } 
+ //END: DRAWGRAPH FUNCTION
+
+  	//BEGIN: AXES - They are the same for all 
+
 	chart1.append("g")
 			.attr("class", "x axis")
 			.attr("transform", "translate(0," + height + ")")
@@ -179,34 +184,137 @@
 			.attr("dy", -80)
 			.text("Incidents");
 
+	
 
-	function mouseoverFunc(d) {
-			d3.select(this)
-				.transition()
-				.style("opacity", 1)
-				.attr("r", 10);
-			tooltip
-				.style("display", null) // this removes the display none setting from it
-				.html("<p>Country: " + d.key + "</p>");
-			}
+	
+	// END: AXES - They are the same for all 
 
-		function mousemoveFunc(d) {
-			tooltip
-				.style("top", (d3.event.pageY - 10) + "px" )
-				.style("left", (d3.event.pageX + 10) + "px");
-			}
+	  function makeCountryData(data) {
 
-		function mouseoutFunc(d) {
-			d3.select(this)
-				.transition()
-				.style("opacity", 1)
-				.attr("r", 4);
-	    tooltip.style("display", "none");  // this sets it to invisible!
+		  var countryByYear = d3.nest()
+	     .key(function(d) { return d.country_txt; })
+	     .sortKeys(d3.ascending)
+	     .key(function(d) {return d.iyear;})
+	     .sortKeys(function(a,b) {
+	     	return d3.ascending(yearFormat.parse(a), yearFormat.parse(b));
+	     })
+	     .rollup(function(leaves) { 
+	     		return { "incidents": leaves.length, 
+	     		"deaths": d3.sum(leaves, function(d) { return +d.nkill; }),
+	     		"country" : leaves[0].country_txt
+	          };
+	      })
+	      .entries(data);
 
-	     }
+		var totalIncidents = d3.nest()
+		  .key(function(d) { return d.country_txt; })
+		  .rollup(function(v) { return v.length; })
+		  .entries(data);
+
+
+    function top50countries(totalIncidents) {
+        return totalIncidents.sort(function (a, b) {
+            return d3.descending(a.values, b.values);
+        }).slice(0,100);
+    }
+
+    var topIncidents = top50(totalIncidents);
+
+   	var topCountriesByIncidentsNames = topIncidents.map(function (d) {return d.key;}); // country names
+
+		var topCountriesForLine = countryByYear.filter(function(d) {
+			return topCountriesByIncidentsNames.indexOf(d.key) !== -1;
+		});
+
+		// structure is key: country, { key: year, values: deaths incident}
+
+
+		var valuesForXScale = d3.merge(topCountriesForLine.map(function(d) {return d.values;}));
+		var valuesForYScale = valuesForXScale.map(function(d) {return d.values;});
+
+		xScale.domain(
+			d3.extent(valuesForXScale, function(d) {
+				return yearFormat.parse(d.key);
+			}));
+
+
+		yScale.domain([
+			d3.max(valuesForYScale, function(d) {
+					return d.incidents;
+			}),
+			0
+			]);
+
+		return topCountriesForLine;
+		}
+
+		function top50(totals) {
+        return totals.sort(function (a, b) {
+            return d3.descending(a.values, b.values);
+        }).slice(0,100);
+    }
+
+		function makeGroupData(data) {
+
+			var groupByYear = d3.nest()
+	     .key(function(d) { return d.gname; })
+	     .sortKeys(d3.ascending)
+	     .key(function(d) {return d.iyear;})
+	     .sortKeys(function(a,b) {
+	     	return d3.ascending(yearFormat.parse(a), yearFormat.parse(b));
+	     })
+	     .rollup(function(leaves) { return { 
+	     	"incidents": leaves.length, 
+	     	"deaths": d3.sum(leaves, function(d) { return +d.nkill; }),
+	     	"group": leaves[0].gname
+	          };
+	      })
+	     .entries(data);
+
+	     console.log(groupByYear);
+
+		var totalGroupIncidents = d3.nest()
+		  .key(function(d) { return d.gname; })
+		  // .key(function(d) { return d.iyear; })
+		  .rollup(function(v) { return v.length; })
+		  .entries(data);
+
+	  console.log(totalGroupIncidents);
+
+    var topIncidents = top50(totalGroupIncidents);
+    console.log(topIncidents);
+
+   	var topGroupsByIncidentsNames = topIncidents.map(function (d) {return d.key;}); // country names
+
+		var topGroupsForLine = groupByYear.filter(function(d) {
+			return topGroupsByIncidentsNames.indexOf(d.key) !== -1;
+		});
+
+		// structure is key: country, { key: year, values: deaths incident}
+
+		var valuesForXScale = d3.merge(topGroupsForLine.map(function(d) {return d.values;}));
+		var valuesForYScale = valuesForXScale.map(function(d) {return d.values;});
+
+		xScale.domain(
+			d3.extent(valuesForXScale, function(d) {
+				return yearFormat.parse(d.key);
+			}));
+
+
+		yScale.domain([
+			d3.max(valuesForYScale, function(d) {
+					return d.incidents;
+			}),
+			0
+			]);
+
+		return topGroupsForLine;
+
+		}
 
 	}
 
 	makeChart1();
+
 
 	}); // end of data csv
